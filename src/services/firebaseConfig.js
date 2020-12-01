c1c68622639a8105c9693c2ef2889cfd79cbda55
oscargross <oscar.gross222@gmail.com>
2020-11-30 21:56:19 -0300

    add firebaseConfig to the project

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,12 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyADLwIrKy9gaZpzMDFJ8TSoq8UodQ9Z-Wg",
+    authDomain: "react-ranking.firebaseapp.com",
+    databaseURL: "https://react-ranking.firebaseio.com",
+    projectId: "react-ranking",
+    storageBucket: "react-ranking.appspot.com",
+    messagingSenderId: "777898632423",
+    appId: "1:777898632423:web:85856147e982db94c2ee42",
+    measurementId: "G-CSP4EQGK7L"
+  };
+
+export default firebaseConfig;
